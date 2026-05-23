158233ca450110cebd78bf24f118424daa47be1e
Kevin Peña <Kevinmati98@gmail.com>
2026-05-23 09:51:34 -0300

    se agrega estructura de clase para el proyecto - Elias Luna elsi1041

---
+++ src/main/java/com/example/Ecoride/EcorideApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcorideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcorideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ecoride/EcorideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcorideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
